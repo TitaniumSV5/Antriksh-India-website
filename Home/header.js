649ef40e90c907a63d46d966d2eede1a28eed9c3
Astro_Bot <autobot268@gmail.com>
2023-07-10 20:22:20 +0530

    landing page add

--- Home/header.js
+++ Home/header.js
@@ -43,6 +43,44 @@ document.addEventListener('DOMContentLoaded', function() {
   var banner = document.querySelector('.banner');
   banner.appendChild(logo);
   banner.appendChild(navbar);
+
+
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+
+// Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+
+// Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+
+// Function to display slides
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1; }
+    if (n < 1) { slideIndex = slides.length; }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+}
+  
+// Automatic slideshow
+  setInterval(function() {
+    plusSlides(1);
+  }, 5000);
+
+
 });
 
 
