1428b369d4021a0b423d379bafa2efd2641fce11
Astro_Bot <autobot268@gmail.com>
2023-07-12 12:01:31 +0530

    Analytics Trial Script 03

--- Home/analytics.js
+++ Home/analytics.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
   // GTM code snippet for the head tag
-  var headCodeSnippet = '\
-    <!-- GTM code for the head tag -->\
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':\
-    new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],\
-    j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=\
-    \'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);})(window,document,\'script\',\'dataLayer\',\'GTM-KWXXGWB\');</script>\
-  ';
+
+  // <!-- Google Tag Manager -->
+  var headCodeSnippet = `
+  
+  (function(w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event: 'gtm.js'
+    });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != 'dataLayer' ? '&l=' + l : '';
+    j.async = true;
+    j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, 'script', 'dataLayer', 'GTM-KWXXGWB');
+
+  `;
+  // <!-- End Google Tag Manager --> 
   
   // GTM code snippet for the body tag
   var bodyCodeSnippet = '\
@@ -22,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
   headScriptElement.innerHTML = headCodeSnippet;
   
   // Create a new script element for the GTM code in the body tag
-  var bodyScriptElement = document.createElement('script');
+  var bodyScriptElement = document.createElement('noscript');
   bodyScriptElement.innerHTML = bodyCodeSnippet;
   
   // Add the script elements to the head and body tags
